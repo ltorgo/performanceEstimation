911320a17d5e3756d435d428a0cf25db26797602
Luis <ltorgo@gmail.com>
2014-11-16 00:30:43 +0000

    New paired tests ready for incorporation.

--- R/newPaired.R
+++ R/newPaired.R
@@ -1,88 +1,97 @@
 
-pcs <-  function(obj,baseline,test="wilcoxon") {
+pcs <-  function(obj,baseline,maxs=rep(FALSE,length(metricNames(obj))),p.val=0.05) {
     if (!inherits(obj,'ComparisonResults')) stop(obj,' is not of class "ComparisonResults".\n')
 
-    if (! (test %in% c('wilcoxon','t.test')))
-        stop("pairedComparison: valid tests are 'wilcoxon' or 't.test'.")
-    else t.func <- switch(test,
-                          wilcoxon="wilcox.test",
-                          t.test="t.test")
-
-
-
-  
+    ## basic information on the expriment
     ts <- taskNames(obj);     nts <- length(ts)
     ws <- workflowNames(obj); nws <- length(ws)
     ms <- metricNames(obj);   nms <- length(ms)
 
-    scores <- vector("list",nms)
-    rks <- vector("list",nms)
-    avgRksWFs <- vector("list",nms)
-    for(p in 1:nms) scores[[p]] <- t(sapply(obj,function(m) sapply(m,function(i) mean(i@iterationsScores[,p],na.rm=TRUE))))
-
-    rks <- lapply(1:nms,function(i) t(apply(scores[[i]],1,rank)))
-    avgRksWFs <- lapply(1:nms,function(i) apply(rks[[i]],2,mean))
-    names(scores) <- names(rks) <- names(avgRksWFs) <- ms
-
-    ## Testing the null hypothesis that all WFs are equivalent
-    xi <- 12*nts/(nws*(nws+1)) * (sapply(avgRksWFs,function(r) sum(r*r)) - (nws*(nws+1)^2)/4)
-    FFs <- (nts-1)*xi / (nts*(nws-1) - xi)
-
-    p.val <- 0.05
-    critVal <- df(1-p.val,nws-1,(nws-1)*(nts-1))
-    rejNull <- FFs > critVal
-
-    ## posthoc tests
-
-    ## the critical difference
-    CD.n <- qtukey(1-p.val,nws,1e06)/sqrt(2)*sqrt(nws*(nws+1)/(6*nts))
-    CD.bd <- qtukey(1-(p.val/(nws-1)),nws,1e06)/sqrt(2)*sqrt(nws*(nws+1)/(6*nts))
-
-    allRkDifs <- outer(avgRksWFs$mse,avgRksWFs$mse,function(x,y) abs(x-y))
-
-
-    
     if (missing(baseline))  # using the first workflow as baseline if none indicated
         baseline <- topPerformer(obj,ms[1],ts[1])@name
-
     other <- setdiff(ws,baseline)
+    pb <- which(ws==baseline)
 
-    ## the results of the paired comparisons against the baseline (will be on first row)
-    res <- array(NA,dim=c(nws,4,nms,nts),
-                 dimnames=list(c(baseline,other),
-                               c("AvgScore","SdScore","Diff","p.value"),
-                               ms,
-                               ts)
-                 )
+    ## this list will hold the results of the comparisons, one for each metric
+    compResults <- vector("list",nms)
+    names(compResults) <- ms
+    
+    #scores <- vector("list",nms)
+    #rks <- vector("list",nms)
+    #avgRksWFs <- vector("list",nms)
+    for(p in 1:nms) {
+        compResults[[p]] <- list()
+        compResults[[p]]$setup <- list(nTasks=nts,nWorkflows=nws)
+        compResults[[p]]$scores <- t(sapply(obj,function(m) sapply(m,function(i) mean(i@iterationsScores[,p],na.rm=TRUE))))
+        compResults[[p]]$rks <- t(apply(if (maxs[p]) -compResults[[p]]$scores else compResults[[p]]$scores,1,rank))
+        compResults[[p]]$avgRksWFs <- apply(compResults[[p]]$rks,2,mean)
 
-
-    for(t in ts) {
-        ## get the results of the baseline
-        res[baseline,1:2,,t] <- t(apply(obj[[t]][[baseline]]@iterationsScores,2,
-                             function(x) c(AvgScore=mean(x,na.rm=TRUE),SdScore=sd(x,na.rm=TRUE))))
-        for(m in ms) {
-            res[baseline,"AvgScore",m,t] <- mean(obj[[t]][[baseline]]@iterationsScores[,m],
-                                                 na.rm=TRUE)
-            res[baseline,"SdScore",m,t] <- sd(obj[[t]][[baseline]]@iterationsScores[,m],
-                                              na.rm=TRUE)
-            ## get the results of the others and compare
-            for(o in other) {
-                #oRes <- obj[[t]][[o]]@iterationsScores
-
-                a <- mean(obj[[t]][[o]]@iterationsScores[,m],na.rm=TRUE)
-                s <- sd(obj[[t]][[o]]@iterationsScores[,m],na.rm=TRUE)
-                tst <- try(w <- do.call(t.func,
-                                        list(obj[[t]][[o]]@iterationsScores[,m],
-                                             obj[[t]][[baseline]]@iterationsScores[,m],
-                                             paired=T))
+        ## Wilcoxon Signed Rank and t-Student tests
+        compResults[[p]]$t.test <- array(NA,dim=c(nws,3,nts),
+                 dimnames=list(c(baseline,other),c("Score","DiffAvgScores","p.value"),
+                     ts)
                          )
-                p <- if (inherits(tst,"try-error"))  NA else tst$p.value
-                res[o,,m,t] <- c(a,s,res[baseline,'AvgScore',m,t]-a,p)
+        compResults[[p]]$WilcoxonSignedRank.test <- array(NA,dim=c(nws,3,nts),
+                 dimnames=list(c(baseline,other),c("Score","DiffAvgScores","p.value"),
+                     ts)
+                         )
+        for(t in ts) {
+            compResults[[p]]$WilcoxonSignedRank.test[baseline,,t] <- NA
+            compResults[[p]]$WilcoxonSignedRank.test[baseline,"Score",t] <- compResults[[p]]$scores[t,baseline]
+            compResults[[p]]$t.test[baseline,,t] <- NA
+            compResults[[p]]$t.test[baseline,"Score",t] <- compResults[[p]]$scores[t,baseline]
+            for(o in other) {
+                tst <- try(wilcox.test(obj[[t]][[o]]@iterationsScores[,p],
+                                       obj[[t]][[baseline]]@iterationsScores[,p],
+                                       paired=T))
+                compResults[[p]]$WilcoxonSignedRank.test[o,"DiffAvgScores",t] <- compResults[[p]]$scores[t,baseline] - compResults[[p]]$scores[t,o]
+                compResults[[p]]$WilcoxonSignedRank.test[o,"Score",t] <- compResults[[p]]$scores[t,o]
+                compResults[[p]]$WilcoxonSignedRank.test[o,"p.value",t] <-
+                    if (inherits(tst,"try-error"))  NA else tst$p.value
+                tst <- try(t.test(obj[[t]][[o]]@iterationsScores[,p],
+                                  obj[[t]][[baseline]]@iterationsScores[,p],
+                                  paired=T))
+                compResults[[p]]$t.test[o,"DiffAvgScores",t] <- compResults[[p]]$scores[t,baseline] - compResults[[p]]$scores[t,o]
+                compResults[[p]]$t.test[o,"Score",t] <- compResults[[p]]$scores[t,o]
+                compResults[[p]]$t.test[o,"p.value",t] <-
+                    if (inherits(tst,"try-error"))  NA else tst$p.value
             }
+        }
+#            names(compResults[[p]]$WilcoxonSignedRank.test$p.values) <- other
+#            names(compResults[[p]]$t.test$p.values) <- other
+        
+        ## Testing the null hypothesis that all WFs are equivalent
+        chi <- 12*nts/(nws*(nws+1)) * (sum(compResults[[p]]$avgRksWFs^2) - (nws*(nws+1)^2)/4)
+        FF <- (nts-1)*chi / (nts*(nws-1) - chi)
+        critVal <- df(1-p.val,nws-1,(nws-1)*(nts-1))
+        rejNull <- FF > critVal
+        compResults[[p]]$F.test <- list(chi=chi,FF=FF,critVal=critVal,rejNull=rejNull)
+        
+        compResults[[p]]$Nemenyi.test <- NA
+        compResults[[p]]$BonferroniDunn.test <- NA
+        if (rejNull) {
+            ## Nemenyi critical difference
+            CD.n <- qtukey(1-p.val,nws,1e06)/sqrt(2)*sqrt(nws*(nws+1)/(6*nts))
+            allRkDifs <- outer(compResults[[p]]$avgRksWFs,compResults[[p]]$avgRksWFs,
+                               function(x,y) abs(x-y))
+            signifDifs <- allRkDifs >= CD.n
+            compResults[[p]]$Nemenyi.test <- list(critDif=CD.n,
+                                                  rkDifs=allRkDifs,
+                                                  signifDifs=signifDifs)
             
+            ## Bonferroni-Dunn test against the baseline
+            
+            ## Bonferroni-Dunn critical difference
+            CD.bd <- qtukey(1-(p.val/(nws-1)),2,1e06)/sqrt(2)*sqrt(nws*(nws+1)/(6*nts))
+            diffs2baseline <- abs(compResults[[p]]$avgRksWFs[-pb]-compResults[[p]]$avgRksWFs[pb])
+            signifDifs <- diffs2baseline >= CD.bd
+            compResults[[p]]$BonferroniDunn.test <- list(critDif=CD.bd,
+                                                         baseline=baseline,
+                                                         rkDifs=diffs2baseline,
+                                                         signifDifs=signifDifs)
+
         }
     }
-    
-    res
+    compResults
 }
 
