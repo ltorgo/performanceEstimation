47087a95ee8386c8ac733728b3cb244f45e5240d
brett <ltorgo@gmail.com>
2015-06-16 12:04:58 +0100

    changing the references of metacost into maxutil

--- R/workflows.R
+++ R/workflows.R
@@ -510,12 +510,12 @@ standardPOST <- function(form,train,test,preds,steps,...) {
             if (any(idx <- preds > pars$supLim)) preds[idx] <- pars$supLim
 
         ## -----------    
-        } else if (s == "metacost") {
+        } else if (s == "maxutil") {
             pars <- list(...)
-            if (is.numeric(train[,tgtVar])) stop("MetaCost is only available for classification tasks.",call.=FALSE)
-            if (!("cb.matrix" %in% names(pars))) stop("MetaCost requires that you specify a cost-benefit matrix.",call.=FALSE)
-            if (is.null(dim(preds))) stop("MetaCost requires that the classifier outputs a matrix of probabilities as predictions.",call.=FALSE)
-            if (ncol(preds) != ncol(pars$cb.matrix)) stop("Error in MetaCost: predictions do not contain as many class probabilities as there are classes in the cost-benefit matrix.",call.=FALSE)
+            if (is.numeric(train[,tgtVar])) stop("'maxutil'' is only available for classification tasks.",call.=FALSE)
+            if (!("cb.matrix" %in% names(pars))) stop("'maxutil' requires that you specify a cost-benefit matrix.",call.=FALSE)
+            if (is.null(dim(preds))) stop("'maxutil' requires that the classifier outputs a matrix of probabilities as predictions.",call.=FALSE)
+            if (ncol(preds) != ncol(pars$cb.matrix)) stop("Error in 'maxutil': predictions do not contain as many class probabilities as there are classes in the cost-benefit matrix.",call.=FALSE)
             ps <- apply(preds,1,function(ps) which.max(apply(pars$cb.matrix,2,function(cs) sum(ps*cs))))
             preds <- levels(train[,tgtVar])[ps]
             
